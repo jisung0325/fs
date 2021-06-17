75da4684cd756c490fa12f1067be77aad63ad56f
jisung <sdh210421@sdh.hs.kr>
2021-06-17 19:52:42 +0900

    happ13r

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => {
-    res.send(`Hello World!`);
-});
+app.set('view engine', 'ejs');
 
+app.use(express.json())
+
+app.use(express.urlencoded())
+
+app.get('/', (req, res) => {
+    res.render(`index`);
+});
+app.get('/test', (req, res) => {
+    res.render(`test`);
+});
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 app.listen(3000, () => {
     console.log("서버 열림!");
 });
